e5fef393e32fb92e858e57ae291b963185d6b063
“Saranyasarma <sivasuthansaranyasarma1@gmail.com>
2025-02-10 13:35:32 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// index.js
+
+const countryCodeData = require("./data/Country_Dialcode.json");
+const countryLocationData = require("./data/Country_Data.json");
+
+module.exports = {
+  countryCodeData,
+  countryLocationData,
+};
